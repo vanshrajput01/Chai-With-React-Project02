319b94084ceb385b79dfe2114fecf756f7088bc9
vanshrajput01 <rajputvansh009@gmail.com>
2024-01-15 12:22:33 +0530

    empty commit

--- Project02/src/App.jsx
+++ Project02/src/App.jsx
@@ -8,6 +8,7 @@ function App() {
     {/* props */}
     <DogCard dogName = "Dog01" image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIsmnD1wgkT3JmU0BkcHDh-Yiw35jsDcw-UVTPI_dyKA&s"/>
     <DogCard dogName = "Dog02" image = "https://i.insider.com/59e5d4a1b0c292755836e078?width=1136&format=jpeg"/>
+    <DogCard dogName = "Dog03" image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgCmgCJf45eJ-VJPLiPFGaJChIahnwEjAqnhYROXOpqA&s"/>
 
     </>
 
