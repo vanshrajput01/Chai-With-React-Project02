571118eb791221faa3475b8a0e11cec55bc8cf5c
vanshrajput01 <rajputvansh009@gmail.com>
2024-01-15 12:27:57 +0530

    final commit in porps

--- Project02/src/App.jsx
+++ Project02/src/App.jsx
@@ -1,10 +1,19 @@
 import { DogCard } from "./DogCard"
 
 function App() {
+  let title = "These are dog images"
 
   return (
+
     <>
+    
     <h1>Project02</h1>
+    <p style={
+      {
+        fontSize : "30px",
+        color : "seagreen"
+      }
+    }>{title}</p>
     {/* props */}
     <DogCard dogName = "Dog01" image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIsmnD1wgkT3JmU0BkcHDh-Yiw35jsDcw-UVTPI_dyKA&s"/>
     <DogCard dogName = "Dog02" image = "https://i.insider.com/59e5d4a1b0c292755836e078?width=1136&format=jpeg"/>
