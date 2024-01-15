99b8a59b1c3295eb219812376f9ab71b755054bc
vanshrajput01 <rajputvansh009@gmail.com>
2024-01-15 12:18:59 +0530

    second commit

--- Project02/src/App.jsx
+++ Project02/src/App.jsx
@@ -6,7 +6,8 @@ function App() {
     <>
     <h1>Project02</h1>
     {/* props */}
-    <DogCard dogName = "Dog01"/>
+    <DogCard dogName = "Dog01" image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIsmnD1wgkT3JmU0BkcHDh-Yiw35jsDcw-UVTPI_dyKA&s"/>
+    <DogCard dogName = "Dog02" image = "https://i.insider.com/59e5d4a1b0c292755836e078?width=1136&format=jpeg"/>
 
     </>
 
--- Project02/src/DogCard.jsx
+++ Project02/src/DogCard.jsx
@@ -3,7 +3,7 @@ export function DogCard(props) {
     return (
         <>
             <h3>{props.dogName}</h3>
-            <Image src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIsmnD1wgkT3JmU0BkcHDh-Yiw35jsDcw-UVTPI_dyKA&s"/>
+            <Image src = {props.image}/>
         </>
     )
 
